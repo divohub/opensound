172d3e780b991089c7bf7a554441471e5c205c30
Fedor Kupryakov <deadmanzad@gmail.com>
2026-03-25 11:35:00 +0300

    feat: added basic recipe reading

--- cli/main.py
+++ cli/main.py
@@ -1,1 +1,21 @@
-# cli/main.py
+import typer
+from rich.console import Console
+import yaml
+from .models import Recipe
+
+console = Console()
+
+app = typer.Typer()
+
+
+@app.command()
+def install(local: str = typer.Option(None, "--local", "-l")):
+    if local:
+        with open(local) as file:
+            data = yaml.safe_load(file)
+            recipe = Recipe(**data)
+            console.print(f"Succesgully loaded: {recipe}")
+
+
+if __name__ == "__main__":
+    app()
