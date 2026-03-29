b7e2a2365fc15a219a1adf6b18bed5a3c5860329
Fedor Kupryakov <deadmanzad@gmail.com>
2026-03-29 18:58:29 +0300

    fix typer sync start for command
    u cant use async for app decorators

--- cli/main.py
+++ cli/main.py
@@ -3,7 +3,7 @@ import typer
 from rich.console import Console
 import yaml
 from .models import Recipe
-from .installer import download_package, install_recipe
+from .installer import install_recipe
 
 console = Console()
 
@@ -11,14 +11,15 @@ app = typer.Typer()
 
 
 @app.command()
-async def install(local: str = typer.Option(None, "--local", "-l")):
+def install(local: str = typer.Option(None, "--local", "-l")):
     if local:
         with open(local) as file:
             data = yaml.safe_load(file)
             recipe = Recipe(**data)
-            console.print(f"Succesgully loaded: {recipe}")
-        await install_recipe(recipe)
+            console.print(f"Succesfully loaded: {recipe}")
+
+        asyncio.run(install_recipe(recipe))
 
 
 if __name__ == "__main__":
-    asyncio.run(app())
+    app()
