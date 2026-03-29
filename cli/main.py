a196641409aa6da19f304cb176db6231a2ac2868
Fedor Kupryakov <deadmanzad@gmail.com>
2026-03-29 15:19:35 +0300

    fix: main async install

--- cli/main.py
+++ cli/main.py
@@ -1,7 +1,9 @@
+import asyncio
 import typer
 from rich.console import Console
 import yaml
 from .models import Recipe
+from .installer import download_package, install_recipe
 
 console = Console()
 
@@ -9,13 +11,14 @@ app = typer.Typer()
 
 
 @app.command()
-def install(local: str = typer.Option(None, "--local", "-l")):
+async def install(local: str = typer.Option(None, "--local", "-l")):
     if local:
         with open(local) as file:
             data = yaml.safe_load(file)
             recipe = Recipe(**data)
             console.print(f"Succesgully loaded: {recipe}")
+        await install_recipe(recipe)
 
 
 if __name__ == "__main__":
-    app()
+    asyncio.run(app())
