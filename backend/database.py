8e390c9b548b88a491068269d4f717200ee06b19
Fedor Kupryakov <pikpoksoound@gmail.com>
2026-03-30 16:19:33 +0300

    add: database session init

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,19 @@
-# backend/database.py
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from backend.config import settings
+from typing import AsyncGenerator
+
+
+engine = create_async_engine(settings.async_database_url, echo=True)
+
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False
+)
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
